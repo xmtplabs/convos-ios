a64f46f7f37bb23302baf9bd0a5bee1d796e550a
Louis Rouffineau <dev@louis.io>
2025-12-02 17:36:36 +0100

    Update XMTPiOS sdk from 4.6.2-rc3 to 4.6.2

--- ConvosCore/Package.swift
+++ ConvosCore/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/groue/GRDB.swift.git", exact: "7.5.0"),
-        .package(url: "https://github.com/xmtp/xmtp-ios.git", exact: "4.6.2-rc3"),
+        .package(url: "https://github.com/xmtp/xmtp-ios.git", exact: "4.6.2"),
         .package(url: "https://github.com/tesseract-one/CSecp256k1.swift.git", from: "0.2.0"),
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.62.2"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.1.0"),
