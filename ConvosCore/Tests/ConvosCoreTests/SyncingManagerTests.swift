a84ee31e735e166eef9b19b6a94c773c03cbd17b
yewreeka <jarodluebbert@gmail.com>
2026-02-25 22:26:10 -0800

    fix: Add findOrCreateDm to test mocks for ConversationsProvider

--- ConvosCore/Tests/ConvosCoreTests/SyncingManagerTests.swift
+++ ConvosCore/Tests/ConvosCoreTests/SyncingManagerTests.swift
@@ -198,6 +198,10 @@ class TestableMockConversations: ConversationsProvider, @unchecked Sendable {
         nil
     }
 
+    func findOrCreateDm(with peerInboxId: String) async throws -> XMTPiOS.Dm {
+        fatalError("not implemented in test mock")
+    }
+
     func streamAllMessages(type: ConversationFilterType,
                           consentStates: [ConsentState]?,
                           onClose: (() -> Void)?) -> AsyncThrowingStream<DecodedMessage, any Error> {
